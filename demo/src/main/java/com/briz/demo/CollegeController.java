e74c66eb63ec4924ab9259b1f010fb2f5dafc1d0
Mr.Randhir <rajkumar951ce@gmail.com>
2023-02-24 09:43:12 +0530

    Spring

--- demo/src/main/java/com/briz/demo/CollegeController.java
+++ demo/src/main/java/com/briz/demo/CollegeController.java
@@ -1,5 +1,16 @@
 package com.briz.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CollegeController {
 
+	@RequestMapping("/hello")
+	
+	public String hello()
+	{
+		return "I am devil of my world";
+	}
+	
 }
